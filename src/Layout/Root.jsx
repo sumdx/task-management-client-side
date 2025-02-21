f8fc339af3b242d33c0af861730f326895612448
sumdx <sumdas74@gmail.com>
2025-02-20 17:53:30 -0800

    tailwind flowbit and dark mode added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import NavBar from '../Components/NavBar';
 
 const Root = () => {
     return (
         <div>
             <h1>Root</h1>
+            <NavBar></NavBar>
             <Outlet></Outlet>
         </div>
     );
