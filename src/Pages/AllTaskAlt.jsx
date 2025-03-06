2de8960fd200c28fe06cfe554843bc08d347cd5e
sumdx <sumdas74@gmail.com>
2025-03-06 14:13:29 -0800

    started working on drag and drop features

---
+++ src/Pages/AllTaskAlt.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllTaskAlt = () => {
+    return (
+        <div>
+            This is new Page for new All task page
+        </div>
+    );
+};
+
+export default AllTaskAlt;
